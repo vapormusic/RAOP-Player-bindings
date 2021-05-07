da089873745fdb55e17c808cd438d54de1c44921
SchlaubiSchlump <Schlaubischlump@users.noreply.github.com>
2021-05-07 21:52:11 +0200

    Add missing file

---
+++ examples/getkey.py
@@ -0,0 +1,30 @@
+"""
+Simple helper function to read keyevents using python.
+"""
+import sys
+
+if sys.platform[:3] == 'win':
+    import msvcrt
+    def getkey():
+        key = msvcrt.getch()
+        if ord(key) == 224: #catch second event with arrow keys
+            key = msvcrt.getch()
+        return key
+else:
+    import termios, sys, os
+    TERMIOS = termios
+
+    def getkey():
+        fd = sys.stdin.fileno()
+        old = termios.tcgetattr(fd)
+        new = termios.tcgetattr(fd)
+        new[3] = new[3] & ~TERMIOS.ICANON & ~TERMIOS.ECHO
+        new[6][TERMIOS.VMIN] = 1
+        new[6][TERMIOS.VTIME] = 0
+        termios.tcsetattr(fd, TERMIOS.TCSANOW, new)
+        c = None
+        try:
+            c = os.read(fd, 1)
+        finally:
+            termios.tcsetattr(fd, TERMIOS.TCSAFLUSH, old)
+        return c
